9809965f593cbc4f98a0f872d8524882c9e7593e
Lucas <32420616+QuixoteCode@users.noreply.github.com>
2025-02-10 21:54:51 +0100

    10-02-2025

--- react-fundamentals/06.js
+++ react-fundamentals/06.js
@@ -1,7 +1,7 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
-import * as React from 'react'
+import React from 'react';
 
 //Without Extra Credits
 /*
@@ -31,8 +31,38 @@ function App() {
 */
 
 //Extra Credits Number 1
-//Copiado y pegado de lo ya hecho
+/*
 function UsernameForm({onSubmitUsername}) {
+
+  const ref = React.useRef(null)
+
+  function handleSubmit(event){
+    event.preventDefault();
+    onSubmitUsername(ref.current.value);
+  }
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="nombreUsuario">Username:</label>
+        <input id="nombreUsuario" type="text" ref={ref}/>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+}
+*/
+
+//Extra Credits Number 2
+/*
+function UsernameForm({onSubmitUsername}) {
+
+  const [error, setError] = React.useState(null);
 
   function handleSubmit(event){
     event.preventDefault();
@@ -40,11 +70,55 @@ function UsernameForm({onSubmitUsername}) {
     onSubmitUsername(valor);
   }
   
+  function handleChange(event){
+    const valorInput = event.target.value;
+    const esValido = valorInput === valorInput.toLowerCase();
+    setError(esValido ? null : 'Username must be lower case')
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="nombreUsuario">Username:</label>
-        <input id="nombreUsuario" type="text" />
+        <input id="nombreUsuario" type="text" onChange={handleChange}/>
+        <p style={{color: "red"}} role="alert">{error}</p>
+      </div>
+      <button disabled={Boolean(error)} type="submit">Submit</button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+}
+*/
+
+//Extra Credits Number 3
+function UsernameForm({onSubmitUsername}) {
+
+  const [username, setUsername] = React.useState(null);
+
+  function handleSubmit(event){
+    event.preventDefault();
+    onSubmitUsername(username);
+  }
+  
+  function handleChange(event){
+    const valorInput = event.target.value;
+    setUsername(valorInput.toLowerCase());
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="nombreUsuario">Username:</label>
+        <input
+          id="nombreUsuario"
+          type="text"
+          value={username}
+          onChange={handleChange}
+        />
       </div>
       <button type="submit">Submit</button>
     </form>
