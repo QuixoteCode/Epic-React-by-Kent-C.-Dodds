201349547df5e1ae687133fac48d2e50d623ceab
Lucas <32420616+QuixoteCode@users.noreply.github.com>
2025-02-16 20:47:37 +0100

    16-02-2025

--- react-hooks/02.js
+++ react-hooks/02.js
@@ -4,8 +4,11 @@
 import * as React from 'react'
 
 //Without Extra Credits
+/*
 function Greeting({initialName = ''}) {
-  const [name, setName] = React.useState(window.localStorage.getItem('name') ?? initialName)
+  const [name, setName] = React.useState(
+    window.localStorage.getItem('name') ?? initialName,
+  )
 
   React.useEffect (() => {
     window.localStorage.setItem('name', name)
@@ -28,6 +31,102 @@ function Greeting({initialName = ''}) {
 
 function App() {
   return <Greeting initialName="Lucas"/>
+}
+*/
+
+//Extra Credits Number 1
+/*
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(
+    () => window.localStorage.getItem('name') ?? initialName,
+  )
+
+  React.useEffect(() => {
+    window.localStorage.setItem('name', name)
+  })
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting initialName="Lucas" />
+}
+*/
+
+//Extra Credits Number 2
+/*
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(
+    () => window.localStorage.getItem('name') ?? initialName,
+  )
+
+  React.useEffect(() => {
+    window.localStorage.setItem('name', name)
+  }, [name]);
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting initialName="Lucas" />
+}
+*/
+
+//Extra Credit Number 3
+function useLocalStorageState(key, defaultValue = ''){
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) ?? defaultValue,
+  )
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state)
+  }, [key, state]);
+
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+
+  const [name, setName] = useLocalStorageState('name', initialName)
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting initialName="Lucas" />
 }
 
 export default App
