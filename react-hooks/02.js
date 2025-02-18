719f14f226a3db60dd87326ddf508edcf4b6ab62
Lucas <32420616+QuixoteCode@users.noreply.github.com>
2025-02-18 20:51:57 +0100

    18-02-2025

--- react-hooks/02.js
+++ react-hooks/02.js
@@ -95,6 +95,7 @@ function App() {
 */
 
 //Extra Credit Number 3
+/*
 function useLocalStorageState(key, defaultValue = ''){
   const [state, setState] = React.useState(
     () => window.localStorage.getItem(key) ?? defaultValue,
@@ -111,6 +112,51 @@ function Greeting({initialName = ''}) {
 
   const [name, setName] = useLocalStorageState('name', initialName)
 
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+*/
+
+//Extra Credits Number 4
+function useLocalStorageState(
+  key,
+  defaultValue = '',
+  {serializar = JSON.stringify, deserializar = JSON.parse} = {},
+) {
+  const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+      return deserializar(valueInLocalStorage)
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+
+  const keyPreviaRef = React.useRef(key)
+
+  React.useEffect(() => {
+    const keyPrevia = keyPreviaRef.current
+    if (keyPrevia !== key) {
+      window.localStorage.removeItem(keyPrevia)
+    }
+    keyPreviaRef.current = key
+    window.localStorage.setItem(key, serializar(state))
+  }, [key, serializar, state])
+
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName)
   function handleChange(event) {
     setName(event.target.value)
   }
