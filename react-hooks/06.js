d3eb39a4db8dbff67135ab36f64fd9331c1998cb
Lucas <32420616+QuixoteCode@users.noreply.github.com>
2025-02-28 18:43:25 +0100

    28-02-2025

--- react-hooks/06.js
+++ react-hooks/06.js
@@ -55,6 +55,7 @@ function App() {
 */
 
 //Extra Credits Number 1
+/*
 import {
   fetchPokemon,
   PokemonInfoFallback,
@@ -100,6 +101,155 @@ function PokemonInfo({pokemonName}) {
   } else {
     return <PokemonDataView pokemon={pokemon} />
   }
+}
+
+function App() {
+  const [pokemonName, setPokemonName] = React.useState('')
+
+  function handleSubmit(newPokemonName) {
+    setPokemonName(newPokemonName)
+  }
+
+  return (
+    <div className="pokemon-info-app">
+      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+      <hr />
+      <div className="pokemon-info">
+        <PokemonInfo pokemonName={pokemonName} />
+      </div>
+    </div>
+  )
+}
+*/
+
+//Extra Credits Number 2
+/*
+import {
+  fetchPokemon,
+  PokemonInfoFallback,
+  PokemonDataView,
+  PokemonForm,
+} from '../pokemon'
+
+import sadFace from './sad-face.png'
+
+function PokemonInfo({pokemonName}) {
+  const [pokemon, setPokemon] = React.useState(null)
+  const [error, setError] = React.useState(null)
+  const [status, setStatus] = React.useState('idle')
+
+  React.useEffect(() => {
+    if (!pokemonName) {
+      return
+    }
+    setStatus('pending')
+
+    fetchPokemon(pokemonName).then(
+      pokemon => {
+        setPokemon(pokemon)
+        setStatus('resolved')
+      },
+      error => {
+        setError(error)
+        setStatus('rejected')
+      },
+    )
+  }, [pokemonName])
+
+  if (status === 'rejected') {
+    return (
+      <div role="alert">
+        There was an error:{' '}
+        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+        <img src={sadFace} alt="sad face" />
+      </div>
+    )
+  } else if (status === 'idle') {
+    return 'Submit a pokemon'
+  } else if (status === 'pending') {
+    return <PokemonInfoFallback name={pokemonName} />
+  } else if (status === 'resolved') {
+    return <PokemonDataView pokemon={pokemon} />
+  }
+
+  throw new Error('This should be impossible!')
+}
+
+function App() {
+  const [pokemonName, setPokemonName] = React.useState('')
+
+  function handleSubmit(newPokemonName) {
+    setPokemonName(newPokemonName)
+  }
+
+  return (
+    <div className="pokemon-info-app">
+      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+      <hr />
+      <div className="pokemon-info">
+        <PokemonInfo pokemonName={pokemonName} />
+      </div>
+    </div>
+  )
+}
+*/
+
+//Extra Credits Number 3
+import {
+  fetchPokemon,
+  PokemonInfoFallback,
+  PokemonDataView,
+  PokemonForm,
+} from '../pokemon'
+
+import sadFace from './sad-face.png'
+
+function PokemonInfo({pokemonName}) {
+  //const [{ pokemon, error, status }, setState] = React.useState({
+    //pokemon: null,
+    //error: null,
+    //status: 'idle',
+  //})
+  const [state, setState] = React.useState({
+    pokemon: null,
+    error: null,
+    status: 'idle',
+  })
+  const { pokemon, error, status } = state
+
+  React.useEffect(() => {
+    if (!pokemonName) {
+      return
+    }
+    setState({status: 'pending' })
+
+    fetchPokemon(pokemonName).then(
+      pokemon => {
+        setState({ pokemon, status: 'resolved'})
+      },
+      error => {
+        setState({ error, status: 'rejected'})
+      },
+    )
+  }, [pokemonName])
+
+  if (status === 'rejected') {
+    return (
+      <div role="alert">
+        There was an error:{' '}
+        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+        <img src={sadFace} alt="sad face" />
+      </div>
+    )
+  } else if (status === 'idle') {
+    return 'Submit a pokemon'
+  } else if (status === 'pending') {
+    return <PokemonInfoFallback name={pokemonName} />
+  } else if (status === 'resolved') {
+    return <PokemonDataView pokemon={pokemon} />
+  }
+
+  throw new Error('This should be impossible!')
 }
 
 function App() {
